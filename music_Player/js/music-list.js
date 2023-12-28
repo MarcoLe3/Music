21c04763d6d7941d70bf761ffea34af30fc8cf5d
Marco Lee <117403240+MarcoLe3@users.noreply.github.com>
2023-12-28 11:28:21 -0800

    Add files via upload

---
+++ music_Player/js/music-list.js
@@ -0,0 +1,31 @@
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+
+let allMusic = [
+    {
+      name: "Seven(feat. Latto)",
+      artist: "Jung kook, Latto",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "ETA",
+      artist: "NewJeans",
+      img: "music-2",
+      src: "music-2",
+
+    },
+    {
+      name: "Tears to Gold",
+      artist: "Faouzia",
+      img: "music-3",
+      src: "music-3",
+    }
+
+  ];
